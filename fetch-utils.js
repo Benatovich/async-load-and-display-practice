e98b1306a016319626828abac8a62a3c190ec91a
Benatovich <sambenatovich@gmail.com>
2021-12-14 15:09:43 -0500

    render-utils should be good

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,17 +1,17 @@
 // this key is the 'anonymous key'. it's okay to expose it publicly as long as we setup 'rls' security in our database
 // a key is kind of like a password
 // this was assigned to us by supabase for this project
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTUwNzY3MywiZXhwIjoxOTU1MDgzNjczfQ.T4j8T8PmMQvcyTO4egZ0EUIxHwmUeklr4DArTmn6mc0';
 
 // similarly, here's the url supabase assigned us for our database
-const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
+const SUPABASE_URL = 'https://cnfgxcsilmdanstiixji.supabase.co';
 
 // finally, here's the client object. this object is how we will get data from our database
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export async function getCars() {
+export async function getRats() {
     const response = await client
-        .from('cars')
+        .from('rats')
         .select(`
         *,
         makes (*)
@@ -20,3 +20,15 @@ export async function getCars() {
     console.log(response.data);
     return response.data;
 }
+
+// export async function getCars() {
+//     const response = await client
+//         .from('cars')
+//         .select(`
+//         *,
+//         makes (*)
+//     `);
+
+//     console.log(response.data);
+//     return response.data;
+// }
---
+++ render-utils.js
@@ -0,0 +1,28 @@
+// In your app, in a separate file, write a render function for your domain. 
+// Your function should accept one object and returns a DOM element. 
+// This function should return at minimum a div with two p tags. The outer div should have a css class of 'rat-item'.
+export function renderRatDetails(rat) {
+    const div = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const colorEl = document.createElement('p');
+    const sexEl = document.createElement('p');
+    const livingEl = document.createElement('p');
+
+    div.classList.add('rat-item');
+
+    nameEl.textContent = rat.name;
+    nameEl.classList.add('name');
+
+    colorEl.textContent = rat.color;
+    colorEl.classList.add('color');
+
+    sexEl.textContent = rat.sex;
+    sexEl.classList.add('sex');
+
+    livingEl.textContent = true ? `${rat.name} is still alive` : `${rat.name} is no longer with us`;
+    livingEl.classList.add('living');
+
+    div.append(nameEl, colorEl, sexEl, livingEl);
+    
+    return div;
+}
